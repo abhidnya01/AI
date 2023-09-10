22052192bc29da415ac43cd9ce94ef9558603dd1
abhidnya01 <138887178+abhidnya01@users.noreply.github.com>
2023-09-10 19:54:00 +0530

    Update PRAC10B(family).pl

--- PRAC10B(family).pl
+++ PRAC10B(family).pl
@@ -19,3 +19,9 @@ father(X,Y):- parent(X,Y),male(X).
 haschild(X):- parent(X,_).
 sister(X,Y):- parent(Z,X),parent(Z,Y),female(X),X\==Y.
 brother(X,Y):-parent(Z,X),parent(Z,Y),male(X),X\==Y.
+
+
+%% | ?-  father(X, Y).
+%% father(X, Y).
+%% X = tom
+%% Y = bob ? 
